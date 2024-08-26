15c121bd4e05fbb5ac6e930128b5f0c71fa31914
Mark Hooijkaas <mark.hooijkaas@vancis.nl>
2024-08-26 11:45:12 +0200

    use specific version v.1.30.4 of kubectl

--- bin/koolbox-install-kubectl.sh
+++ bin/koolbox-install-kubectl.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/bash
 
+# install latest stable
+# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# install specific version
+curl -LO "https://dl.k8s.io/release/v1.30.4/bin/linux/amd64/kubectl"
 
-export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=DontWarn
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
-apt-get update
-apt-get -y install --no-install-recommends kubectl
-rm -rf /var/lib/apt/lists/*
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
+chmod a+r /etc/bash_completion.d/kubectl
+rm kubectl
+
+# Old Ubuntu way
+#export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=DontWarn
+#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+#echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+#apt-get update
+#apt-get -y install --no-install-recommends kubectl
+#rm -rf /var/lib/apt/lists/*
