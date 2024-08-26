7c099edad2998ba25eb3c230071ab965c3133fd3
Mark Hooijkaas <mark.hooijkaas@vancis.nl>
2024-08-26 11:47:00 +0200

    use version 2.17.37 of aws cli

--- bin/koolbox-install-aws-cli.sh
+++ bin/koolbox-install-aws-cli.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/bash
 
-pip3 install awscli --upgrade
+# pip3 install awscli --break-system-packages
+#apt-get -y install --no-install-recommends awscli
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.17.37.zip" -o "awscliv2.zip"
+# latest
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+ls -la *.zip aws
+./aws/install
+rm -rf awscliv2.zip aws
