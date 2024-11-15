fa6827c03adb86e75ee61a92afc7507375a6a269
Mark Hooijkaas <mark.hooijkaas@vancis.nl>
2024-11-15 12:21:53 +0100

    Improved installer to support both user and system installation, and conform to XDG

--- bin/koolbox-install-kubectl.sh
+++ bin/koolbox-install-kubectl.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/bash
 
+source $(dirname "${BASH_SOURCE[0]}")/koolbox-init-install.inc
+
+
 # install latest stable
 # curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 # install specific version
-curl -LO "https://dl.k8s.io/release/v1.30.4/bin/linux/amd64/kubectl"
+#curl -LO "https://dl.k8s.io/release/${koolbox_kubectl_version}/bin/linux/amd64/kubectl"
 
-install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
-chmod a+r /etc/bash_completion.d/kubectl
-rm kubectl
+koolbox_install_file_version kubectl ${koolbox_kubectl_version}
+if ${koolbox_install_as_root}; then
+    $KOOLBOX_INSTALL_DIR/kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
+    sudo chmod a+r /etc/bash_completion.d/kubectl
+else
+    $KOOLBOX_INSTALL_DIR/kubectl completion bash >$(koolbox_install_completions_dir)/kubectl
+fi
+#rm kubectl
 
 # Old Ubuntu way
 #export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=DontWarn
