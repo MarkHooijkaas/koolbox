fb92e170cbdcb2647d2e4ccc50fd3d4ceb46913d
Mark Hooijkaas <mark@hooijkaas.net>
2025-08-16 10:46:11 +0200

    added new kloolbox builder

--- koolbox-files/bin/koolbox-install-kubectl-bash-completion.sh
+++ koolbox-files/bin/koolbox-install-kubectl-bash-completion.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+source $(dirname "${BASH_SOURCE[0]}")/koolbox-init-install.inc
+
 # install completions
-elif ${koolbox_install_asoot}; then
+if ${KOOLBOX_INSTALL_AS_ROOT}; then
     $KOOLBOX_INSTALL_BIN_DIR/kubectl completion bash | $KOOLBOX_SUDO_CMD tee /etc/bash_completion.d/kubectl > /dev/null
     $KOOLBOX_SUDO_CMD chmod a+r /etc/bash_completion.d/kubectl
 else
