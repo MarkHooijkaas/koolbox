3f023ca897fcf1390a0b796f0eadb87b2896c206
Mark Hooijkaas <mark@hooijkaas.net>
2025-08-16 14:59:36 +0200

    fixed aws-cli and azure-cli for kloudbox

--- koolbox-files/bin/koolbox-install-aws-cli.sh
+++ koolbox-files/bin/koolbox-install-aws-cli.sh
@@ -3,9 +3,12 @@
 # pip3 install awscli --break-system-packages
 #apt-get -y install --no-install-recommends awscli
 
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-2.17.37.zip" -o "awscliv2.zip"
-# latest
-#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+if [[ -z ${KOOLBOX_INSTALL_AWS_CLI_VERSION:-} ]]; then
+    # latest
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+else
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_${KOOLBOX_INSTALL_AWS_CLI_VERSION}.zip" -o "awscliv2.zip"
+fi
 
 unzip -q awscliv2.zip
 ./aws/install
--- koolbox-files/bin/koolbox-install-azure-cli.sh
+++ koolbox-files/bin/koolbox-install-azure-cli.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/bash
+source $(dirname "${BASH_SOURCE[0]}")/koolbox-init-install.inc
 
-script=/tmp/install-azure-cli.bash
+# Based on https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-linux?view=azure-cli-latest&pivots=apt
+# apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
 
-curl -L https://aka.ms/InstallAzureCli >$script
+${KOOLBOX_SUDO_CMD} mkdir -p /etc/apt/keyrings
+curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
+  gpg --dearmor | ${KOOLBOX_SUDO_CMD} tee /etc/apt/keyrings/microsoft.gpg > /dev/null
+${KOOLBOX_SUDO_CMD} chmod go+r /etc/apt/keyrings/microsoft.gpg
 
-sed -i 's/< $_TTY//' $script
-bash $script
-rm $script
+
+AZ_DIST=$(lsb_release -cs)
+echo "Types: deb
+URIs: https://packages.microsoft.com/repos/azure-cli/
+Suites: ${AZ_DIST}
+Components: main
+Architectures: $(dpkg --print-architecture)
+Signed-by: /etc/apt/keyrings/microsoft.gpg" | ${KOOLBOX_SUDO_CMD} tee /etc/apt/sources.list.d/azure-cli.sources
+
+${KOOLBOX_SUDO_CMD} apt-get update
+${KOOLBOX_SUDO_CMD} apt-get install azure-cli
