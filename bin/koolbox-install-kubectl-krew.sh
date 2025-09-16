621aff7be5326be77377a9a4fe1ee4f4dbbb9579
Mark Hooijkaas <mark@hooijkaas.net>
2025-09-16 16:42:34 +0200

    fixed install krew, since it works very differently

--- bin/koolbox-install-kubectl-krew.sh
+++ bin/koolbox-install-kubectl-krew.sh
@@ -12,7 +12,7 @@ install_result_path=${KREW_ROOT}/bin/kubectl-krew
 function download_krew() {
     local os="$(uname | tr '[:upper:]' '[:lower:]')"
     local arch="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')"
-    local download_file="krew-${os}_${arch}"
+    download_file="krew-${os}_${arch}"
 
     koolbox_verbose downloading ${download_file}
     mkdir -p ${KREW_ROOT}
@@ -23,14 +23,17 @@ function download_krew() {
     # tar gives errors, because it has some MacOS specific values
     tar zxf "${download_file}.tar.gz" 2>/dev/null || true
 
-    koolbox_verbose installing ${download_file} into ${download_result_path}
-    chmod 755 ${download_file}
-    touch ${download_file}
-    mkdir -p bin
-    mv ${download_file} bin/kubectl-krew
 }
 
 function install_krew() {
+    if [[ -f ${download_file-} ]]; then
+        cmd=./${download_file}
+    else
+        cmd=$install_result_path
+    fi
+    koolbox_verbose running ${cmd} install krew
+    ${cmd} install krew
+
     koolbox_verbose cleaning up remaining files
     rm -f ${KREW_ROOT}/.??* ${KREW_ROOT}/*.tar.gz
 }
