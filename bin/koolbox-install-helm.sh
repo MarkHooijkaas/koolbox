e4af7387928a3619dff7d0daa43d459eb131ea5f
Mark Hooijkaas <mark@hooijkaas.net>
2025-09-16 23:55:51 +0200

    use new install functions for helm

--- bin/koolbox-install-helm.sh
+++ bin/koolbox-install-helm.sh
@@ -1,9 +1,33 @@
 #!/usr/bin/bash
+set -ue
 
+source $(dirname "${BASH_SOURCE[0]}")/koolbox-init.inc
+koolbox_parse_options "${@}"
 
-# Install helm3
+tool_name=helm
+install_result_path=$KOOLBOX_INSTALL_BIN_DIR/helm
 
-mkdir /tmp/helm
-wget -qO- "https://get.helm.sh/helm-v3.5.3-linux-amd64.tar.gz" | tar xvz -C /tmp/helm/
-mv /tmp/helm/linux-amd64/helm /usr/local/bin/
-rm -rf /tmp/helm
+function init_install_helm() {
+    : ${KOOLBOX_INSTALL_HELM_VERSION:=${KOOLBOX_TOOL_VERSION:-}}
+    if [[ -z ${KOOLBOX_INSTALL_HELM_VERSION} ]]; then
+        echo helm version not specified using -V option or KOOLBOX_INSTALL_HELM_VERSION var
+        exit 1
+    fi
+    helm_dir=$KOOLBOX_INSTALL_OPT_DIR/helm/${KOOLBOX_INSTALL_HELM_VERSION}
+    download_result_path=$helm_dir/linux-amd64/helm
+}
+
+function download_helm() {
+    mkdir -p ${helm_dir}/bin/
+    wget -qO- "https://get.helm.sh/helm-${KOOLBOX_INSTALL_HELM_VERSION}-linux-amd64.tar.gz" | tar xvz -C $helm_dir
+}
+
+function install_helm() {
+    install_with_link
+}
+
+function install_helm_completions() {
+    ${download_result_path} completion bash >${KOOLBOX_INSTALL_BASH_COMPLETE_DIR}/helm
+}
+
+main_install "${@}"
