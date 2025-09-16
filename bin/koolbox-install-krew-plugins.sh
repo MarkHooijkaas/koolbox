82fe4c03ee4668884bee6aebdba81876d87ed520
Mark Hooijkaas <mark@hooijkaas.net>
2025-09-17 00:10:04 +0200

    Added install-krew-plugins script

---
+++ bin/koolbox-install-krew-plugins.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -ue
+
+source $(dirname "${BASH_SOURCE[0]}")/koolbox-init.inc
+
+export KREW_ROOT=${KREW_ROOT:-${KOOLBOX_INSTALL_OPT_DIR}/krew}
+
+
+if [[ ! -f ${KREW_ROOT}/bin/kubectl-krew ]]; then
+    echo KREW_ROOT not set correctly or krew not installed
+    exit 1
+fi
+
+if [[ -z ${KOOLBOX_INSTALL_KREW_PLUGINS:-} ]]; then
+    echo no plugins specified in KOOLBOX_INSTALL_KREW_PLUGINS
+    exit 1
+else
+    kubectl krew install ${KOOLBOX_INSTALL_KREW_PLUGINS}
+fi
