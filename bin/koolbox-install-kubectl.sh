0fb1450f027239fcb6ba55937a606b3186e3065b
Mark Hooijkaas <mark@hooijkaas.net>
2025-09-16 13:32:48 +0200

    cleanup of init and install-kubectl

--- bin/koolbox-install-kubectl.sh
+++ bin/koolbox-install-kubectl.sh
@@ -1,26 +1,36 @@
 #!/usr/bin/bash
+set -ue
 
-source $(dirname "${BASH_SOURCE[0]}")/koolbox-init-install.inc
-
-
+source $(dirname "${BASH_SOURCE[0]}")/koolbox-init.inc
+koolbox_parse_options "${@}"
 
 if [[ -z ${KOOLBOX_INSTALL_KUBECTL_VERSION:-} ]]; then
-    # install latest stable
-    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-    chmod 755 kubectl
-    KOOLBOX_INSTALL_KUBECTL_VERSION=$(./kubectl version --client|grep ^Client| sed 's/Client Version: v//')
-    echo installed kubectl version $KOOLBOX_INSTALL_KUBECTL_VERSION
-else
-    # install specific version
-    curl -LO "https://dl.k8s.io/release/${KOOLBOX_INSTALL_KUBECTL_VERSION}/bin/linux/amd64/kubectl"
+    KOOLBOX_INSTALL_KUBECTL_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
 fi
-koolbox_install_file_version kubectl ${KOOLBOX_INSTALL_KUBECTL_VERSION}
-rm kubectl
 
-# Old Ubuntu way
-#export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=DontWarn
-#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-#echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
-#apt-get update
-#apt-get -y install --no-install-recommends kubectl
-#rm -rf /var/lib/apt/lists/*
+install_file=$KOOLBOX_INSTALL_OPT_DIR/kubectl/$KOOLBOX_INSTALL_KUBECTL_VERSION/bin/kubectl
+bin_file=$KOOLBOX_INSTALL_BIN_DIR/kubectl
+
+if [[ -f $install_file ]]; then
+    koolbox_info Existing file $install_file, not downloading again
+else
+    koolbox_info downloading $install_file
+    mkdir -p $KOOLBOX_INSTALL_OPT_DIR/kubectl/$KOOLBOX_INSTALL_KUBECTL_VERSION/bin/
+    curl -L -o $install_file  "https://dl.k8s.io/release/${KOOLBOX_INSTALL_KUBECTL_VERSION}/bin/linux/amd64/kubectl"
+    chmod 755 $install_file
+fi
+
+if [[ -f $bin_file ]]; then
+    koolbox_info already a version of kubectl installed $bin_file
+    if $KOOLBOX_VERBOSE; then
+        ls -l $bin_file
+    fi
+else
+    koolbox_info linking $install_file $bin_file
+    ln -s $install_file $bin_file
+fi
+
+if ${KOOLBOX_INSTALL_COMPLETIONS:-true}; then
+    koolbox_info installing bash completions in ${KOOLBOX_INSTALL_BASH_COMPLETE_DIR}/koolbox-kubectl
+    $install_file completion bash >${KOOLBOX_INSTALL_BASH_COMPLETE_DIR}/koolbox-kubectl
+fi
