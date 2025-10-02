431ac3bfecbe4035bf1a38799cf0f8e4b026d3ed
Mark Hooijkaas <mark@hooijkaas.net>
2025-10-02 11:48:06 +0200

    use new flow for kubectl

--- install-tools/koolbox-install-kubectl.sh
+++ install-tools/koolbox-install-kubectl.sh
@@ -1,36 +1,22 @@
 #!/usr/bin/bash
-set -ue
-
 source $(dirname "${BASH_SOURCE[0]}")/koolbox-init.inc
-koolbox_parse_options "${@}"
-
 
 tool_name=kubectl
-install_result_path=$KOOLBOX_INSTALL_BIN_DIR/kubectl
 
-init_install_kubectl() {
-    : ${KOOLBOX_INSTALL_KUBECTL_VERSION:=${KOOLBOX_TOOL_VERSION:-}}
-    if [[ -z ${KOOLBOX_INSTALL_KUBECTL_VERSION} ]]; then
-        KOOLBOX_INSTALL_KUBECTL_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
-    fi
-    tool_version=${KOOLBOX_INSTALL_KUBECTL_VERSION}
-    download_filename=vault_${tool_version}_${OS_ARCH}.zip
+init_vars() {
+    calc_transformed_os_arch
+    download_filename=kubectl
     download_url="https://dl.k8s.io/release/${KOOLBOX_INSTALL_KUBECTL_VERSION}/bin/${OS}/${ARCH}/kubectl"
-    init_download_dir
-    download_result_path=${download_dir}/kubectl
 }
 
-download_kubectl() {
-    dry-run curl -L -o ${download_result_path} "https://dl.k8s.io/release/${KOOLBOX_INSTALL_KUBECTL_VERSION}/bin/${OS}/${ARCH}/kubectl"
-    dry-run chmod 755 ${download_result_path}
+extract_downloaded_file() {
+    dry-run cp ${download_path} ${extract_path}
+    dry-run chmod 755  ${extract_path}
 }
 
-install_kubectl() {
-    install_with_link
+
+install_extras() {
+    dry-run ${extract_path} completion bash >${completion_path}
 }
 
-install_kubectl_completions() {
-    dry-run ${download_result_path} completion bash >${KOOLBOX_INSTALL_BASH_COMPLETE_DIR}/koolbox-kubectl
-}
-
-main_install "${@}"
+run_all "${@}"
